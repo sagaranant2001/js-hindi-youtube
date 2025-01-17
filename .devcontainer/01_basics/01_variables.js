e4aee7a275a2206f7c333d3510147fa3babc937e
sagaranant2001 <80241101+sagaranant2001@users.noreply.github.com>
2025-01-17 12:02:09 +0000

    conatiner config done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,8 +1,14 @@
-const accountId= 144553
-let accountEmail ="anant@gmail.com"
-var accountPassword="12345"
-accountCity="Delhi"
+const accountId= 144553 // const is used when constant value is given, means that the value cannot be changed.//
 
-accountId= 2
+let accountEmail ="anant@gmail.com" /*let variable is used as it uses the concept of scope that 
+ means it has fixed scope*/
+var accountPassword="12345" 
+/* Prefer not to use var because of issue in block scope and fuctioanal scope(this will be taught later)*/ 
+accountCity="Delhi"/* You can assign values to variable without giving let and const. But it is not prefer to use*/ 
+
+// accountId= 2
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+/* this means that using [] we can print multiple variables in one console.table statements insaed of using multiple
+ console.log lines..................*/
 
 console.log(accountId);
---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,55 @@
+"use strict" // it is used that tells that all the JS code is treated as newer version //
+
+ alert(3+3) /*If we run this line in nodejs is will throw error as it has different syntax for running 
+ this line in nodejs
+ But if we run this line in browser then it will run fine and give output as 6  
+ */
+console.log(3+3); //this is prefrable //
+console.log(3   // this is not prefrable
+    +3);        // CODE REDABILITY SHOULD BE HIGH,GOOD AND SUSTAINABLE//
+ 
+ 
+    /* TO READ ABOUT JS STANDARDS
+PREFER TWO SITES
+1.)developer.mozzila.org (https://developer.mozilla.org/en-US/docs/Web/API/Window/alert)
+ // easy to understand and uses simple language //
+2.) tc39.es ECMAScript  (https://tc39.es/ecma262/2023/multipage/)
+// Uses high level language
+*/
+
+let name ="Anant" // STRING Datatype
+let age =20  // INT Datatype
+let isLoggedIn = true //BOOLEAN Datatype
+
+///========================PRIMITIVE DATATYPE=============================================////
+// number int => range is 2 to power 52 or 53 //
+// bigint => used for very big numbers //
+// string => "" and '' both can be used........Usually prefer "" only //
+// boolean => return true or false //
+// null => standlone value for example
+
+let state =null;
+console.log(state); // it will give output as null //
+
+let empty;
+console.log(empty); // it will give output as undefined //
+
+// symbol => gives unique value usually used in reactjs //
+
+//================================NON PRIMTIVE DATATYPE==================================///
+// object 
+
+/* typeof-- It means that it gives typeof value for example if we ask "anant" is 
+of what type so  it will give String*/
+ console.log(typeof "anant"); // String
+console.log(typeof 34); //  number 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+
+
+
+ 
+
+
+
+ 
