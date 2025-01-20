bfe15a488a4a7f27680d506f30993a8e103b55bf
sagaranant2001 <80241101+sagaranant2001@users.noreply.github.com>
2025-01-20 16:49:36 +0000

    config done successfully

---
+++ .devcontainer/datatypes_summary.js
@@ -0,0 +1,17 @@
+//PRIMITIVE DATATYPE (call by value)//
+//These are of 7 types : String,Number,Boolean,null,undefined,Symbol,BigINT
+
+//***************************NOTE************************************/
+/*JavaScript is dynamic language and non static, which means that variables
+can hold vaues of different types during runtime 
+FOR EXAMPLE:- const score=1   and const socre= false (here we didn't told datatype)
+    (as if int number in 1st case and boolean value in 2nd case)*/
+ //        BUT IN JAVA 
+ /*IN Java, JAVA is statically typed language, as it expects its variable
+ to be declared before they can be assigned value 
+                  int number =2;
+          and boolean number = false (here we told the datatype) 
+
+
+// NON PRIMITIVE DATATYPE (refrence type)
+// Types: Array,Objects,Functions
